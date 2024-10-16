79e88e4cb72667b35d70c0db21bb077113f02415
LUIS GERARDO AGUILAR GARCIA <eagledluis.game@gmail.com>
2024-10-16 18:43:27 -0500

    fix: footer

--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -6,31 +6,34 @@ const Social = () => {
     <div className="Project_Container" id="Socials">
       <div className="Project_Title"> Contact</div>
       <div className="DISPLAY_FLEX">
-        <div>
+        <div className="DISPLAY_FLEX">
           <a
             href="mailto:luisgerardo900@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
           >
             <AiOutlineMail className="ICON2" />
+            <div className="ICON2TEXT">E-mail</div>
           </a>
         </div>
-        <div>
+        <div className="DISPLAY_FLEX">
           <a
             href="https://www.linkedin.com/in/luisgaguilarg/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <AiOutlineLinkedin className="ICON2" />
+            <div className="ICON2TEXT">LinkedIn</div>
           </a>
         </div>
-        <div>
+        <div className="DISPLAY_FLEX">
           <a
             href="https://github.com/LuisAguilarDev"
             target="_blank"
             rel="noopener noreferrer"
           >
             <AiFillGithub className="ICON2" />
+            <div className="ICON2TEXT">GitHub</div>
           </a>
         </div>
       </div>
