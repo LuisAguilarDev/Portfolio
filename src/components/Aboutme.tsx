ef854b9a9cb15c940264381c3fdb8ee54b1f5eeb
LUIS GERARDO AGUILAR GARCIA <100291529+LuisAguilarDev@users.noreply.github.com>
2025-02-11 13:41:49 -0500

    fix: about

--- src/components/Aboutme.tsx
+++ src/components/Aboutme.tsx
@@ -4,7 +4,7 @@ import Section from './Section';
 export default function Aboutme() {
   const { t } = useTranslation();
   return (
-    <Section title={t('about.title')} id="about">
+    <Section className="pb-10 px-3" title={t('about.title')} id="about">
       <div className="flex items-center justify-center">
         <div className="flex flex-col w-[65ch] text-white">
           <p>{t('about.content1')}</p>
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,10 +1,23 @@
 import { ReactElement } from 'react'
 
-export default function Section({title,children,id}:Readonly<{title:string,children:ReactElement,id:string}>) {
-    return (
-      <section className="w-full Project_Container" id={id}>
-        {title && <h2 className="Project_Title">{title}</h2>}
-        {children}
-      </section>
-    );
+export default function Section({
+  title,
+  children,
+  id,
+  className,
+}: Readonly<{
+  title: string;
+  children: ReactElement;
+  id: string;
+  className?: string;
+}>) {
+  return (
+    <section
+      className={`w-full Project_Container ${className ? className : ''}`}
+      id={id}
+    >
+      {title && <h2 className="Project_Title">{title}</h2>}
+      {children}
+    </section>
+  );
 }
